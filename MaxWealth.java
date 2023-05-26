0a0fcd3db7f1362cb1c6d733b299265ecdf2e27b
Kashish-Shukla18 <kashish.shukla.8076@gmail.com>
2023-05-26 21:57:58 +0530

    Search In 2D array

---
+++ MaxWealth.java
@@ -0,0 +1,24 @@
+package DSA;
+
+public class MaxWealth {
+    public static void main(String[] args) {
+        int[][] accounts={{1,5,3},{3,2,1}};
+        System.out.println(maximumwealth(accounts));
+    }
+    public static int maximumwealth(int[][] accounts){
+//        person - row
+//        account - col
+        int ans=Integer.MIN_VALUE;
+        for (int person=0;person<accounts.length;person++){
+//            while starting new col, take sum for that row
+            int sum=0;
+            for (int account=0;account<accounts[person].length;account++){
+                sum+=accounts[person][account];
+            }
+            if (sum>ans){
+                ans=sum;
+            }
+        }
+        return ans;
+    }
+}
