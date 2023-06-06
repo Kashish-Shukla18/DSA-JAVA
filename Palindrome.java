3938784a63f82819d2b9c3343793852cb1b040af
Kashish-Shukla18 <kashish.shukla.8076@gmail.com>
2023-06-06 16:30:33 +0530

    To check that the given String is palindrome or not

---
+++ Palindrome.java
@@ -0,0 +1,23 @@
+package DSA;
+
+public class Palindrome {
+    public static void main(String[] args) {
+        String name="abcdcba";
+        System.out.println(isPalindrome(name));
+
+    }
+    static boolean isPalindrome(String str){
+        str=str.toLowerCase();
+
+        for (int i=0;i<str.length()/2;i++){
+            char start=str.charAt(i);
+            char end=str.charAt(str.length()-1);
+            if (start!=end){
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+}
