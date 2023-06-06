25b4794b2cbd1469392ed2c5882ba92fc133e0be
Kashish-Shukla18 <kashish.shukla.8076@gmail.com>
2023-06-06 16:41:45 +0530

    To check that the given String is palindrome or not

--- Palindrome.java
+++ Palindrome.java
@@ -2,19 +2,20 @@ package DSA;
 
 public class Palindrome {
     public static void main(String[] args) {
-        String name="abcsba";
+        String name = "abcsba";
         System.out.println(isPalindrome(name));
 
     }
-    static boolean isPalindrome(String str){
-        if (str==null || str.length()==0){
-            return  true;
+
+    static boolean isPalindrome(String str) {
+        if (str == null || str.length() == 0) {
+            return true;
         }
-        str=str.toLowerCase();
-        for (int i=0;i<=str.length()/2;i++){
-            char start=str.charAt(i);
-            char end=str.charAt(str.length()-1-i);
-            if (start!=end){
+        str = str.toLowerCase();
+        for (int i = 0; i <= str.length() / 2; i++) {
+            char start = str.charAt(i);
+            char end = str.charAt(str.length() - 1 - i);
+            if (start != end) {
                 return false;
             }
         }
