2b1c9cf16bc0b40f2edcc7032eebcf9b1d183211
Kashish-Shukla18 <kashish.shukla.8076@gmail.com>
2023-06-06 16:32:56 +0530

    To check that the given String is palindrome or not

--- Palindrome.java
+++ Palindrome.java
@@ -7,8 +7,10 @@ public class Palindrome {
 
     }
     static boolean isPalindrome(String str){
+        if ( str==null || str.length()==0){
+            return  true;
+        }
         str=str.toLowerCase();
-
         for (int i=0;i<str.length()/2;i++){
             char start=str.charAt(i);
             char end=str.charAt(str.length()-1);
