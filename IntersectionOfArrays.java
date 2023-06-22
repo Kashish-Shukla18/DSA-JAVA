fd720c7a89fa3ecb400655b4a0791a415a5b25aa
Kashish-Shukla18 <kashish.shukla.8076@gmail.com>
2023-06-22 19:44:19 +0530

    Intersection of two arrays using HashSet

---
+++ IntersectionOfArrays.java
@@ -0,0 +1,25 @@
+package DSA;
+
+import java.util.HashSet;
+
+public class IntersectionOfArrays {
+    public static void main(String[] args) {
+        int[] arr1={10,15,20,15,30,30,5};
+        int[] arr2={30,5,30,80};
+        System.out.println(intersection(arr1,arr2,7,4));
+    }
+    static int intersection(int[] a,int[] b,int m,int n) {
+        HashSet<Integer> s = new HashSet<Integer>();
+        for(int i = 0; i < m; i++)
+            s.add(a[i]);
+            int res = 0;
+            for (int j = 0; j < n; j++) {
+                if (s.contains(b[j])) {
+                    res++;
+                    s.remove(b[j]);
+                }
+            }
+            return res;
+    }
+
+}
